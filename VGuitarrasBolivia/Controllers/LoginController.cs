75bad81b73733aecf4933d5df774de08d9408829
Jhonatan_FF <jhonats284@gmail.com>
2023-12-20 02:09:56 -0400

    adicion del Login

---
+++ VGuitarrasBolivia/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SQLitePCL;
+using VGuitarrasBolivia.Context;
+
+namespace VGuitarrasBolivia.Controllers
+{
+    public class LoginController : Controller
+    {
+        private MiContext _context;
+        public LoginController(MiContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string correo,string contrasena)
+        {
+            var usuario = await _context.Usuarios
+                                        .Where(x => x.Email == correo && x.Password == contrasena)
+                                        .FirstOrDefaultAsync();
+            if (usuario != null) 
+            {
+                return RedirectToAction("Index","Home");
+            }
+            else
+            {
+                TempData["LoginError"] = "Cuenta o Contraseña incorrecta!";
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
